21c88b0e7f306e3992fd1263d788862ed33f080e
Jang Rush <weakish@gmail.com>
2020-11-25 23:43:36 +0800

    :new: setup script

---
+++ script/setup.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to access %q: %v\n", path, err)
+			return err
+		}
+		if info.IsDir() {
+			if info.Name() == ".git" || info.Name() == "script" {
+				return filepath.SkipDir
+			}
+			return nil
+		}
+		if info.Name() == "README.md" {
+			return nil
+		}
+		dest := "$HOME" + strings.TrimPrefix(path, ".")
+		fmt.Printf("install -D -p %q %q\n", path, dest)
+		return nil
+	})
+}
